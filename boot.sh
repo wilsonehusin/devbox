211ed7d2146ed2f7c3ef6ea4e722aba389131d61
Wilson Husin <wilson@husin.dev>
2026-05-09 10:58:35 -0700

    boot.sh: install jj

--- boot.sh
+++ boot.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+whoami
+
 user=${CODER_USER:-dev}
 base_url="https://raw.githubusercontent.com/wilsonehusin/devbox/refs/heads/main"
 
@@ -14,3 +16,27 @@ done
 [[ -f "/home/${user}/.bash_profile" ]] || cat <<EOF >"/home/${user}/.bash_profile"
 [[ -s "\$HOME/.bashrc" ]] && source "\$HOME/.bashrc"
 EOF
+
+arch=$(uname -m)
+rustarch=""
+case "${arch}" in
+  "x86_64")
+    rustarch="x86_64"
+    ;;
+  "arm64")
+    rustarch="aarch64"
+    ;;
+  *)
+    rustarch="skip"
+    ;;
+esac
+
+if [[ "${rustarch}" != "skip" ]]; then
+  jj_version="0.41.0"
+  curl -Lo /tmp/jj.tar.gz "https://github.com/jj-vcs/jj/releases/download/v${jj_version}/jj-v${jj_version}-${rustarch}-unknown-linux-musl.tar.gz"
+  tar -zxvf /tmp/jj.tar.gz jj
+  chown "${user}:${user}" jj
+  mv jj /usr/local/bin/jj
+
+  jj --version
+fi
