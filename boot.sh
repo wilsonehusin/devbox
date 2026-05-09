35804c414df770ea323006a9a1e10c8b8acc0874
Wilson Husin <wilson@husin.dev>
2026-05-09 11:26:14 -0700

    boot.sh: install jj to ~/.local/bin

--- boot.sh
+++ boot.sh
@@ -2,18 +2,15 @@
 
 set -euo pipefail
 
-whoami
-
-user=${CODER_USER:-dev}
 base_url="https://raw.githubusercontent.com/wilsonehusin/devbox/refs/heads/main"
 
 dotfiles=("bashrc" "vimrc")
 for f in "${dotfiles[@]}"; do
-  curl -o "/home/${user}/.${f}" "${base_url}/dot/${f}"
-  chown "${user}:${user}" "/home/${user}/.${f}" 
+  curl -o "${HOME}/.${f}" "${base_url}/dot/${f}"
+  chown "${USER}:${USER}" "${HOME}/.${f}" 
 done
 
-[[ -f "/home/${user}/.bash_profile" ]] || cat <<EOF >"/home/${user}/.bash_profile"
+[[ -f "${HOME}/.bash_profile" ]] || cat <<EOF >"${HOME}/.bash_profile"
 [[ -s "\$HOME/.bashrc" ]] && source "\$HOME/.bashrc"
 EOF
 
@@ -35,8 +32,6 @@ if [[ "${rustarch}" != "skip" ]]; then
   jj_version="0.41.0"
   curl -Lo /tmp/jj.tar.gz "https://github.com/jj-vcs/jj/releases/download/v${jj_version}/jj-v${jj_version}-${rustarch}-unknown-linux-musl.tar.gz"
   tar -zxvf /tmp/jj.tar.gz ./jj
-  chown "${user}:${user}" ./jj
-  mv ./jj /usr/local/bin/jj
-
-  jj --version
+  mkdir -p "${HOME}/.local/bin"
+  mv ./jj "${HOME}/.local/bin/"
 fi
